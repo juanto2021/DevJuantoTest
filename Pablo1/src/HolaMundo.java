27517aedde7a0b0123923e40044db87f060dddb3
Juanto <utopick@gmail.com>
2021-01-22 19:58:06 +0100

    Pruebas

---
+++ Pablo1/src/HolaMundo.java
@@ -0,0 +1,27 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// Este es un programa de pruebas ....
+//		System.out.println("Hola Papico");
+//		
+//		System.out.println ("adios Pablo");
+//		
+//		String nombrepablo="Pablo";
+//		
+//		int edad = 27;
+//		
+//		final double PI = 3.14;
+//		
+//		System.out.println ("Hola "+nombrepablo+" tiene "+edad+" años.");
+		
+		for (int i = 0; i < 5; i= i+1) {
+			  System.out.println("La vuelta " +i);
+			  if (i==2) {
+				  System.out.println("Ojo: ews un 2");
+			  }
+			}
+		
+	}
+
+}
